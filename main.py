9b5c5e7d3d19eb51498d21de47b8828def8164e7
VasiaZozulia <vasiazozulia@Mac-Pro-Mac.local>
2025-06-15 14:30:52 +0300

    Added variables for the game

--- main.py
+++ main.py
@@ -14,3 +14,39 @@
 Завершення гри:
           Якщо користувач вичерпав усі спроби, програма виводить повідомлення про поразку і розкриває загаданe число.
 """
+import random
+
+# Define the range within which the number will be guessed
+guess_range = (1, 100)
+
+# Set the maximum number of attempts the user has to guess the number
+tries = 7
+
+# Dictionary containing hints for the user's guesses
+tips = {
+    'small': 'Your number is too small',
+    'big': 'Your number is too hight',
+    'win' : 'Congratulations, you won the game!',
+    'loss' : 'Unfortunately, you lost the game. All your attempts were unsuccessful.',
+    'input_guess_message' : 'Enter your assumption: ',
+    'range_error' : 'The range within which the number will be guessed is incorrect. The first value should be smaller than the second value.',
+    'input_number_error' : 'Please enter the whole number!',
+}
+
+# Randomly select a number between the specified range
+random_number = random.randint(guess_range[0], guess_range[1])
+
+print ("Welcome to the game 'Guess the Number.'")
+print (f"I’ve picked a number between {guess_range[0]} and {guess_range[1]}. You have {tries} tries to guess it. Let's play!")
+
+# Checking if the guess range is within the correct bounds. "
+if guess_range[0] < guess_range[1]:
+    # Start the game
+    while tries > 0:
+        try:
+            guess = int(input(tips['input_guess_message']))
+        except ValueError:
+            print (tips['input_number_error'])
+            continue
+else:
+    print (tips['range_error'])
